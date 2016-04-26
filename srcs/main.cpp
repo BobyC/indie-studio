7e2ecda25207bb8ed7593aec461922b427771d06
mollon_m <maxime.mollonguet@epitech.eu>
2016-04-26 17:43:22 +0200

    main

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,10 +1,17 @@
 //
 // main.cpp for srcs in /home/rigola_s/rendu/C++/cpp_indie_studio/srcs
-// 
+//
 // Made by Rigolat Sébastien
 // Login   <rigola_s@epitech.net>
-// 
+//
 // Started on  Mon Apr 25 19:16:43 2016 Rigolat Sébastien
-// Last update Mon Apr 25 19:16:43 2016 Rigolat Sébastien
+// Last update Tue Apr 26 17:42:45 2016 Maxime Mollonguet
 //
 
+# include <iostream>
+
+int	main()
+{
+  std::cout << "BOSSE PLUS QUE ÇA !!! SASHA & NANA VA NOUS CARRY LOL JPP" << std::endl;
+  return (0);
+}
