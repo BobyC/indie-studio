23d7f546fd99ced33e095d5cd2824fa75e81a7a1
drozdz_b <axel1.drozdzynski@epitech.eu>
2016-06-03 17:42:10 +0200

    Improve Plan class

--- srcs/Plan.cpp
+++ srcs/Plan.cpp
@@ -5,7 +5,7 @@
 ** Login   <drozdz_b@epitech.net>
 **
 ** Started on  Wed Jun 01 16:08:39 2016 drozdz_b
-** Last update Fri Jun 03 17:24:50 2016 drozdz_b
+** Last update Fri Jun 03 17:39:47 2016 drozdz_b
 */
 
 #include "Plan.hpp"
@@ -19,12 +19,15 @@ Plan::Plan(scene::ISceneNode *node, scene::ISceneManager *smgr)
 Plan::Plan(scene::ISceneManager* smgr, video::IVideoDriver * driver)
 : Object(NULL)
 {
+  core::dimension2d<f32> tileSize(1, 1);
+  core::dimension2d<u32>   tileCount(50, 50);
   scene::IMesh			*plane =
     smgr->getGeometryCreator()->createPlaneMesh(tileSize, tileCount);
   _node = smgr->addMeshSceneNode(plane);
 
   if(_node)
   {
+    _node->setMaterialFlag(video::EMF_LIGHTING, false);
     _node->setMaterialTexture(0, driver->getTexture("WoodChips01_D.tga"));
   }
 }
