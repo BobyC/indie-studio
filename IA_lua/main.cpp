3d4e78dc8921a88430b5805ac6a21d9a0d6355ca
mollon_m <maxime.mollonguet@epitech.eu>
2016-06-03 22:14:21 +0200

    push IA in lua + encapsulation

---
+++ IA_lua/main.cpp
@@ -0,0 +1,19 @@
+//
+// main.cpp for  in /home/mollon_m/rendu/test-lua
+//
+// Made by Maxime Mollonguet
+// Login   <mollon_m@epitech.net>
+//
+// Started on  Fri Jun  3 19:36:06 2016 Maxime Mollonguet
+// Last update Fri Jun  3 19:37:08 2016 Maxime Mollonguet
+//
+
+#include "IAController.hh"
+
+int	main(void)
+{
+  IAController	l;
+
+  l.result();
+  return (0);
+}
